0f857c53bfbcd444d4f1da45291cc918dc0ff8ee
Renato Isidio <renatosis@corp.globo.com>
2016-12-09 15:34:23 -0200

    https://github.com/dcapwell/gossh/issues/1

--- ssh.go
+++ ssh.go
@@ -1,7 +1,7 @@
 package cloudstack
 
 import (
-	"code.google.com/p/go.crypto/ssh"
+	"golang.org/x/crypto/ssh"
 	"fmt"
 	"github.com/mitchellh/multistep"
 	packerssh "github.com/mitchellh/packer/communicator/ssh"
